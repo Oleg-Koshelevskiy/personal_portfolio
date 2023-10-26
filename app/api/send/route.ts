9728cf9fe683f9abef38f443f96ddc8aed40208a
Oleg <oleg17.k@gmail.com>
2023-10-26 16:29:20 +0300

    fixed route error

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,19 +1,23 @@
 import { EmailTemplate } from "@/components/email-template";
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import { Resend } from "resend";
+import * as React from "react";
 
-const resend = new Resend(process.env.RESEND_KEY);
-const fromEmail = process.env.FROM_EMAIL;
+const resend = new Resend(process.env.RESEND_API_KEY);
+// const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST(req: any) {
+export async function POST(req: NextRequest) {
   const { email, subject, message } = await req.json();
   console.log(email, subject, message);
   try {
     const data = await resend.emails.send({
-      from: fromEmail,
+      from: "Your Name <sender@domain.com>",
       to: ["oleg17.k@gmail.com"],
       subject: subject,
-      react: EmailTemplate({ subject: subject, message: message }),
+      react: EmailTemplate({
+        subject: subject,
+        message: message,
+      }) as React.ReactElement,
     });
     return NextResponse.json(data);
   } catch (error) {
