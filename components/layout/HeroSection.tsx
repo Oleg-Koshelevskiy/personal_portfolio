4a7781a1b403bf79a325f8f681b9680cd4a52af8
Oleg <oleg17.k@gmail.com>
2023-10-29 15:21:22 +0200

    changed cv

--- components/layout/HeroSection.tsx
+++ components/layout/HeroSection.tsx
@@ -47,8 +47,8 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="/Junior_RoadMap.pdf"
-              download="Example-PDF-document"
+              href="/cv.pdf"
+              download="My_CV"
               target="_blank"
               rel="noreferrer"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
